9c9e33b69a7a4f9c8ea880c60e06d949d1abfc7b
Hanashiko <129686115+Hanashiko@users.noreply.github.com>
2024-04-18 19:01:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from random import choice
+
+def generate_password(length: int) -> str:
+  symbols: str = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]|\\;:'\",.<>/?"
+  password: str = ""
+  for _ in range(length):
+    password += choice(symbols)
+  return password
+
+def main() -> None:
+	length: int = int(input("Введіть довжину пароля: "))
+	password: str = generate_password(length)
+	print(f"Згенерований пароль: {password}")
+
+if __name__ == "__main__":
+	main()
